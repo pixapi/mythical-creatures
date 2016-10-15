a8aa211b06266df337cff4a1e1e238763e1df57e
Amara Lovato <amaracuenca@gmail.com>
2016-10-14 18:34:03 -0600

    finish vampire.rb

--- vampire.rb
+++ vampire.rb
@@ -1,9 +1,23 @@
 class Vampire
-  def initialize(name)
-    @name = "Dracula"
+  def initialize(name, pet = "bat")
+    @name = name
+    @pet = pet
+    @thirsty = true
   end
 
   def name
     @name
+  end
+
+  def pet
+    @pet
+  end
+
+  def thirsty?
+    @thirsty
+  end
+
+  def drink
+    @thirsty = false
   end
 end
